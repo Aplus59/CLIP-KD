8845136201c20bbd0949c9d72588c629eecc3c55
khanh <baokhanhnguyenho@gmail.com>
2026-05-26 11:46:33 +0200

    add script to transfer

---
+++ script/fathomnet/train_teacher_hp_transfer_aug.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+cd src
+
+LEARNING_RATES=(1e-5 5e-6 1e-6)
+WEIGHT_DECAYS=(0.1 0.2)
+
+export USE_AUG=1
+
+for LR in "${LEARNING_RATES[@]}"; do
+    for WD in "${WEIGHT_DECAYS[@]}"; do
+        TAG="fathomnet-transfer-aug-lr${LR}-wd${WD}"
+        echo "Running Transfer Learning (With Aug): LR=${LR}, WD=${WD}"
+        
+        torchrun --nproc_per_node 1 -m \
+            training.main \
+            --pretrained openai \
+            --save-frequency 1 \
+            --zeroshot-frequency 1 \
+            --report-to tensorboard \
+            --train-data="../newd/db/train_split.csv"  \
+            --val-data="../newd/db/val_split.csv"  \
+            --data-root ../newd/images/ \
+            --val-data-root ../newd/images/ \
+            --csv-img-key file_name \
+            --csv-caption-key text \
+            --csv-separator "," \
+            --warmup 200 \
+            --batch-size=128 \
+            --lr=${LR} \
+            --wd=${WD} \
+            --epochs 20 \
+            --workers=4 \
+            --model ViT-B-16 \
+            --logs ../logs/ \
+            --tag ${TAG}
+    done
+done
