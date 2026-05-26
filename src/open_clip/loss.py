41a1e0dbd4c5041fb79472e66296a76c481e70a9
khanh <baokhanhnguyenho@gmail.com>
2026-05-26 13:39:39 +0200

    fix code to run in 1 gpu

--- src/open_clip/loss.py
+++ src/open_clip/loss.py
@@ -248,6 +248,10 @@ class KDClipLoss(nn.Module):
     def forward(self, image_features, text_features, logit_scale, \
         t_image_features, t_text_features, t_logit_scale):
         device = image_features.device
+        
+        normalized_image_features = F.normalize(image_features, dim=1)
+        normalized_text_features = F.normalize(text_features, dim=1)
+        
         if self.world_size > 1:
             all_image_features, all_text_features = gather_features(
                 image_features, text_features,
@@ -259,8 +263,6 @@ class KDClipLoss(nn.Module):
             t_logits_per_image = t_logit_scale * t_all_image_features @ t_all_text_features.T
             t_logits_per_text = t_logits_per_image.T
 
-            normalized_image_features = F.normalize(image_features, dim=1)
-            normalized_text_features = F.normalize(text_features, dim=1)
             normalized_all_image_features = F.normalize(all_image_features, dim=1)
             normalized_all_text_features = F.normalize(all_text_features, dim=1)
             
@@ -271,6 +273,17 @@ class KDClipLoss(nn.Module):
                 logits_per_image = logit_scale * normalized_all_image_features @ normalized_all_text_features.T
                 logits_per_text = logits_per_image.T
         else:
+            all_image_features = image_features
+            all_text_features = text_features
+            t_all_image_features = t_image_features
+            t_all_text_features = t_text_features
+            
+            t_logits_per_image = t_logit_scale * t_all_image_features @ t_all_text_features.T
+            t_logits_per_text = t_logits_per_image.T
+            
+            normalized_all_image_features = normalized_image_features
+            normalized_all_text_features = normalized_text_features
+
             logits_per_image = logit_scale * normalized_image_features @ normalized_text_features.T
             logits_per_text = logit_scale * normalized_text_features @ normalized_image_features.T
 
